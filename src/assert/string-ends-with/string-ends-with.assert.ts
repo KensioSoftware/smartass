abab5a5afb175b9cb67ec6eadf836af46610b1f3
Hugh Grigg <hugh@kensiosoftware.co.uk>
2026-06-11 19:38:45 +0100

    feat: stringEndingWith

--- src/assert/string-ends-with/string-ends-with.assert.ts
+++ src/assert/string-ends-with/string-ends-with.assert.ts
@@ -1,5 +1,6 @@
 import { AssertionError } from "../../assertion-error.js";
 import { desc, repr } from "../../describe/describe.js";
+import { stringEndingWith } from "./string-ends-with.match.js";
 
 /**
  * Assert that a string ends with a given suffix, with type narrowing.
@@ -9,12 +10,13 @@ export function assertStringEndsWith<const T extends string>(
   suffix: T,
   message?: string,
 ): asserts value is `${string}${T}` {
-  if (!value.endsWith(suffix)) {
+  const matcher = stringEndingWith(suffix);
+  if (!matcher.matches(value)) {
     throw new AssertionError(
       message ??
         `Expected ${desc(value)} to end with ${repr(suffix)}, but it did not.`,
       value,
-      `string ending with ${repr(suffix)}`,
+      matcher.represent(),
     );
   }
 }
