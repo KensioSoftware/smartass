8d4a16b45dda95ac02cfc6a5bf78973d7b2716a2
Hugh Grigg <hugh@kensiosoftware.co.uk>
2026-06-12 09:39:40 +0100

    feat: typeFunction

--- src/assert/type-function/type-function.assert.ts
+++ src/assert/type-function/type-function.assert.ts
@@ -1,5 +1,6 @@
 import { AssertionError } from "../../assertion-error.js";
 import { desc } from "../../describe/describe.js";
+import { typeFunction } from "./type-function.match.js";
 
 /**
  * Assert that a value is of type function, with type-narrowing.
@@ -9,7 +10,8 @@ export function assertTypeFunction(
   message = `Expected ${desc(value)} to be of type function.`,
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 ): asserts value is Function {
-  if (typeof value !== "function") {
-    throw new AssertionError(message, typeof value, "function");
+  const matcher = typeFunction();
+  if (!matcher.matches(value)) {
+    throw new AssertionError(message, value, matcher.represent());
   }
 }
