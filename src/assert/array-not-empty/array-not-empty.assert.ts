4018a59565ea0350ac3e8b1feff450783922cb78
Hugh Grigg <hugh@kensiosoftware.co.uk>
2026-06-15 10:27:19 +0100

    feat: improve exposed types from assertArrayNotEmpty

--- src/assert/array-not-empty/array-not-empty.assert.ts
+++ src/assert/array-not-empty/array-not-empty.assert.ts
@@ -1,22 +1,35 @@
-import { assertNonNullable } from "../non-nullable/non-nullable.assert.js";
 import { nonEmptyArray } from "./array-not-empty.match.js";
 import { AssertionError } from "../../assertion-error.js";
+import { desc } from "../../describe/describe.js";
 import type { NonEmptyArray } from "./array-not-empty.type.js";
+
+export function assertArrayNotEmpty<TArray extends unknown[]>(
+  value: TArray,
+  message?: string,
+): asserts value is TArray & NonEmptyArray<TArray[number]>;
+
+export function assertArrayNotEmpty(
+  value: unknown,
+  message?: string,
+): asserts value is NonEmptyArray<unknown>;
 
 /**
  * Assert that an array has at least one element, with type-narrowing.
  */
-export function assertArrayNotEmpty<T>(
-  value: readonly T[] | undefined | null,
-  message?: string,
-): asserts value is NonEmptyArray<T> {
-  assertNonNullable(value, message);
-
+export function assertArrayNotEmpty(value: unknown, message?: string): void {
   if (!nonEmptyArray().matches(value)) {
     throw new AssertionError(
-      message ?? `Expected array not to be empty, but it was empty.`,
+      message ?? buildArrayNotEmptyMessage(value),
       value,
       ["..."],
     );
   }
+}
+
+function buildArrayNotEmptyMessage(value: unknown): string {
+  if (!Array.isArray(value)) {
+    return `Expected ${desc(value)} to be a non-empty array.`;
+  }
+
+  return `Expected array not to be empty, but it was empty.`;
 }
